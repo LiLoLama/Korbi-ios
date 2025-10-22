cc3ea283d18fa425b05bfc14ea8570c271b0c2bb
Liam Schmid <mail@liamschmid.com>
2025-10-22 19:02:54 +0200

    Add files via upload

---
+++ Korbi/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Korbi
+//
+//  Created by Liam Schmid on 22.10.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
